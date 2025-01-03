19799234b07270d8429e63faf630ba724be821cc
laurapena290805 <penaloza.laura@correounivalle.edu.co>
2025-01-03 17:24:29 -0500

    Funcionalidad con estio

--- src/services/api.js
+++ src/services/api.js
@@ -1,49 +1,39 @@
 import {auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail, fetchSignInMethodsForEmail, sendEmailVerification,db, doc, getDoc, getDocs, collection, setDoc, updateDoc, deleteDoc, addDoc, query, where, onSnapshot} from "./firebase";
 
-const collectionName = "books";
+const collectionName = "personas";
 
-//create
-export const createBook = async (obj) => {
-    const colRef = collection(db, 'books');
-    const data = await addDoc(colRef, obj);
-    return data.id;
+// Create con ID personalizado
+export const createPersona = async (id, obj) => {
+    const docRef = doc(db, collectionName, id);
+    await setDoc(docRef, obj); // setDoc en lugar de addDoc para un ID personalizado
 }
 
-//update
-export const updateBook = async (id, obj) => {
+// Update
+export const updatePersona = async (id, obj) => {
     const docRef = doc(db, collectionName, id);
     await updateDoc(docRef, obj);
 }
 
-
-//read
-export const getBooks = async () => {
+// Read todas las personas
+export const getPersonas = async () => {
     const colRef = collection(db, collectionName);
     const result = await getDocs(query(colRef));
     return getArrayFromCollection(result);
 }
 
-
-//Read with where
-export const getBooksByTitle = async (title) => {
-    const colRef = collection(db, collectionName);
-    const result = await getDocs(query(colRef, where("title", "==", title)));
-    return getArrayFromCollection(result);
-}
-
-
-export const getItemById = async (id) => {
+// Read por ID
+export const getPersonaById = async (id) => {
     const docRef = doc(db, collectionName, id);
     const docSnap = await getDoc(docRef);
     return docSnap.data();
 }
 
-//delete
-export const deleteBook = async (id) => {
+// Delete
+export const deletePersona = async (id) => {
     const docRef = doc(db, collectionName, id);
     await deleteDoc(docRef);
 }
 
 const getArrayFromCollection = (collection) => {
-    return collection.docs.map(doc => ({id: doc.id, ...doc.data()}));
-}
+    return collection.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+};
